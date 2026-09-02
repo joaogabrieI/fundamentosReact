a1dfa857cffca97a1dcbddd9e5775a033ad0684c
JOAO GABRIEL DO NASCIMENTO ALVES <joao.gnalves@senacsp.edu.br>
2026-09-01 22:02:36 -0300

    fundamentosReact aula #1 part3

--- fundamentosReact/src/components/events.jsx
+++ fundamentosReact/src/components/events.jsx
@@ -3,6 +3,14 @@ const Events = () => {
         console.log(e)  
         console.log('Ativou o evento!')
     }
+
+    const renderAlgumaCoisa = (x) => {
+        if (x) {
+            return <h1>Renderização 1</h1>
+        } else {
+            return <h1>Renderização 2</h1>
+        }
+    }
     return (
         <div>
             <div>
@@ -12,8 +20,10 @@ const Events = () => {
                 <button onClick={() => console.log('Clicou!')}>Clique aqui também</button>
             </div>
             <div>
-                <button>Clique aqui também2</button>
+                <button >Clique aqui também2</button>
             </div>
+            {renderAlgumaCoisa(false)}      
+            {renderAlgumaCoisa(true)}
         </div>
     )
 
